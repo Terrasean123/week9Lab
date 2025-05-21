d981eb3c88316666c16b69c6dbf7fe0600b1aba8
Sean Makoka <makokatinse@gmail.com>
2025-05-21 16:58:56 +1000

    start

---
+++ cars.php
@@ -0,0 +1,27 @@
+<?php
+require_once "settings.php"
+$dbconn = @mysqli_connect($host,$user,$pwd,$sql_db);
+if($dbconn)
+{
+
+    $query = "SELECT * FROM cars";
+    $result = mysqli_query($dbconn,$query);
+    if($result){
+    while ($row = mysqli_fetch_assoc($result)) {
+    echo "<tr>";
+    echo "<td>" . $row['car_id'] . "</td>";
+    echo "<td>" . $row['make'] . "</td>";
+    echo "<td>" . $row['model'] . "</td>";
+    echo "<td>" . $row['price'] . "</td>";
+    echo "<td>" . $row['yom'] . "</td>";
+    echo "</tr>";
+}
+  
+    }
+    else{}
+    mysqli_close($dbconn);
+}else {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+?>
---
+++ settings.php
@@ -0,0 +1,8 @@
+
+<?php
+    $host = "localhost";         // because XAMPP runs the server locally
+    $user = "root";          // default username for XAMPP's MySQL
+    $pwd = "";              // default password is empty in XAMPP
+    $sql_db = "exhibition_db";  // replace with the actual name of your database
+?>
+
